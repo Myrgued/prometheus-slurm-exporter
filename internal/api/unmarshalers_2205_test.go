b2e3e933bc01a7d152c8d2324f7977aec16492ba
Myriam Guedey <myriam.guedey@ki.uni-stuttgart.de>
2025-03-05 15:46:35 +0100

    Add 22.05

---
+++ internal/api/unmarshalers_2205_test.go
@@ -0,0 +1,46 @@
+//go:build 2205
+
+package api
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/Myrgued/prometheus-slurm-exporter/internal/util"
+)
+
+func TestUnmarshalDiagResponse(t *testing.T) {
+	var r DiagResp
+	fb := util.ReadTestDataBytes("V0036_slurmdbd_diag.json")
+	err := json.Unmarshal(fb, &r)
+	if err != nil {
+		t.Fatalf("failed to unmarshal diag response: %v\n", err)
+	}
+}
+
+func TestUnmarshalJobsResponse(t *testing.T) {
+	var r JobsResp
+	fb := util.ReadTestDataBytes("V0036_slurmdbd_get_jobs.json")
+	err := json.Unmarshal(fb, &r)
+	if err != nil {
+		t.Fatalf("failed to unmarshal jobs response: %v\n", err)
+	}
+}
+
+func TestUnmarshalNodesResponse(t *testing.T) {
+	var r NodesResp
+	fb := util.ReadTestDataBytes("V0036_slurmctld_get_nodes.json")
+	err := json.Unmarshal(fb, &r)
+	if err != nil {
+		t.Fatalf("failed to unmarshal nodes response: %v\n", err)
+	}
+}
+
+func TestUnmarshalPartitionsResponse(t *testing.T) {
+	var r PartitionsResp
+	fb := util.ReadTestDataBytes("V0036_slurmctld_get_partitions.json")
+	err := json.Unmarshal(fb, &r)
+	if err != nil {
+		t.Fatalf("failed to unmarshal partition response: %v\n", err)
+	}
+}
